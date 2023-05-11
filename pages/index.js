49362a6a8df1880faf0151b755ee74b237eda0d1
Luis Flores <luisflores@Luiss-MacBook-Air.local>
2023-05-11 13:46:32 -0700

    Added more description

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, my name is Luis Carlos and I'm a software developer that loves to build beautiful web apps</p>
+        <p>Hello, my name is Luis Carlos and I'm a software developer that loves to build beautiful web apps. Having a look into nextjs</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
